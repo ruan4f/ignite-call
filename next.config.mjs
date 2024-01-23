1fa0e3f47656f5c4b70b54c80ca8a423f9f3610b
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2024-01-22 22:49:35 -0300

    React Study - Next Js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+}
 
-export default nextConfig;
+export default nextConfig
