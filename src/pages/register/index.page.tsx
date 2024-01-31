5898034cef8d1a7afb085f2683aaae3d4d7b5a91
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2024-01-30 21:43:41 -0300

    React Study - NextJs

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -8,6 +8,7 @@ import { Container, Form, FormError, Header } from './styles'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { api } from '@/lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -49,7 +50,12 @@ export default function Register() {
         username: data.username,
       })
     } catch (err) {
-      console.log(err)
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+
+      console.error(err)
     }
   }
 
